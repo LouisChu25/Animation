8136d4da187796a7fa3174da57b184b15c527a54
TimothéePopesco <Timothee.popesco@edu.devinci.fr>
2021-11-10 11:14:52 +0100

    essai animation pingouin au scroll

---
+++ script.js
@@ -0,0 +1,9 @@
+        //Tentative au scroll 
+
+// var pingui = document.getElementsByClassName('.cscroll')[0];
+
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY > 500) {
+//         pingui.classList.add('scroll');
+//     }
+// });
